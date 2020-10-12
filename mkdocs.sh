2c71b2fc137c4f70ea402fcc8385d6ffae0d4306
Greg ORIOL <greg@gregoriol.net>
2020-10-12 14:59:58 +0200

    Another attempt to fix docs folder

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 php get.php
-cp index.html docs/
+cat index.html | sed -E 's#"node_modules/[^"]*/([^"]*)"#"\1"#g' > docs/index.html
 
-mkdir -p docs/node_modules/normalize.css/
-cp node_modules/normalize.css/normalize.css docs/node_modules/normalize.css/normalize.css
-mkdir -p docs/node_modules/c3/
-cp node_modules/c3/c3.min.css docs/node_modules/c3/c3.min.css
+cp node_modules/normalize.css/normalize.css docs/normalize.css
+cp node_modules/c3/c3.min.css docs/c3.min.css
 
-mkdir -p docs/node_modules/d3/dist/
-cp node_modules/d3/dist/d3.min.js docs/node_modules/d3/dist/d3.min.js
-mkdir -p docs/node_modules/c3/
-cp node_modules/c3/c3.min.js docs/node_modules/c3/c3.min.js
-mkdir -p docs/node_modules/tinycolor2/dist/
-cp node_modules/tinycolor2/dist/tinycolor-min.js docs/node_modules/tinycolor2/dist/tinycolor-min.js
+cp node_modules/d3/dist/d3.min.js docs/d3.min.js
+cp node_modules/c3/c3.min.js docs/c3.min.js
+cp node_modules/tinycolor2/dist/tinycolor-min.js docs/tinycolor-min.js
 
 cp index.css docs/
 cp index.js docs/
